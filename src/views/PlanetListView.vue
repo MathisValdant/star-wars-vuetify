dfd2ebbf59f2b4b6ecf3c8808311e567fd19081a
MathisValdant <62253704+MathisValdant@users.noreply.github.com>
2022-12-09 09:50:51 +0100

    Ajout bouton random planet

--- src/views/PlanetListView.vue
+++ src/views/PlanetListView.vue
@@ -6,11 +6,12 @@ import PlanetCard from "../components/PlanetCard.vue";
   <main>
     <v-container>
       <h1 class="ma-2 mb-1">{{ titre }}</h1>
-      <p>
+      <p class="mb-4">
         L'Univers Star Wars regorge de mondes aussi luxuriants que dangereux.
         Voici la liste des planètes que vous pouvez visiter dans cette galaxie
         lointaine, très lointaine...
       </p>
+      <v-btn color="primary" @click="random()"> Planète aléatoire </v-btn>
       <v-row v-if="inProgress">
         <v-col cols="12" class="my-5 text-center">
           <v-progress-circular
@@ -23,7 +24,6 @@ import PlanetCard from "../components/PlanetCard.vue";
           </v-progress-circular>
         </v-col>
       </v-row>
-
       <v-row class="my-3 align-items-stretch justify-center">
         <v-col
           v-for="(planet, i) of infos.results"
@@ -54,7 +54,6 @@ import PlanetCard from "../components/PlanetCard.vue";
       </v-row>
     </v-container>
   </main>
-  <!-- TODO : Système de filtre -->
 </template>
 
 <script>
@@ -66,6 +65,7 @@ export default {
       inProgress: true,
       nbPages: 1,
       curPage: parseInt(this.$route.query.page) || 1,
+      randomPage: "",
     };
   },
   methods: {
@@ -91,6 +91,55 @@ export default {
       this.inProgress = true;
       this.$router.push({ query: { ...this.$route.query, page: curPage } });
       this.getPlanets();
+    },
+    async random() {
+      // On génère un nb aléatoire compris entre 1 et le nb de planète
+      const randomId = Math.ceil(Math.random() * this.infos.count);
+      // Si l'id de la planète est dans la page actuelle
+      if (randomId <= 10) {
+        // On boucle dans chaque planète
+        for (const planet of this.infos.results) {
+          // On split l'URL pour récupérer l'id
+          const compUrl = planet.url.split("/");
+          const id = parseInt(compUrl[5]);
+          // Si la planète à le même id que le random alors on redirige
+          if (randomId === id) {
+            this.$router.push({
+              name: "planet",
+              params: { nom: "this.infos.results.name", id: id },
+            });
+          }
+        }
+        // Si la planète est dans une autre page
+      } else {
+        // On crée une variable qui sera l'id de la page de requête de l'api
+        // On enlève 1 car une page comprend les planètes de 1 à 10
+        let randomIdPage = randomId - 1;
+        // On cherche à récupérer la dizaine de l'id
+        randomIdPage = randomIdPage.toString().split("");
+        let pageId = randomIdPage[0];
+        pageId = parseInt(pageId) + 1;
+        pageId = pageId.toString();
+        // On requête l'api dans la page qui contient la planète
+        try {
+          const result = await fetch(
+            `https://swapi.dev/api/planets?page=${pageId}`
+          );
+          this.randomPage = await result.json();
+          for (const planet of this.randomPage.results) {
+            const compUrl = planet.url.split("/");
+            const id = parseInt(compUrl[5]);
+            if (randomId === id) {
+              this.$router.push({
+                name: "planet",
+                params: { nom: "this.infos.results.name", id: id },
+              });
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
     },
   },
   created() {
