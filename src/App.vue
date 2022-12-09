d75856cc685fe5d9bcca3425d9ccb65f5c7fbab0
MathisValdant <62253704+MathisValdant@users.noreply.github.com>
2022-12-09 12:55:35 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ export default {
       } else if (!navigator.onLine && !this.isDisconnect) {
         this.isDisconnect = true;
       }
-      console.log(navigator.onLine, this.isDisconnect);
     }
   },
   mounted() {
